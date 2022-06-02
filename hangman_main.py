fe0fd8fb2df9e7cce6a2dc3662ea38b1e93fc532
Sumit <sciencebuddy51@gmail.com>
2022-06-02 18:24:47 +0530

    Initial commit

---
+++ hangman_main.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun  1 17:21:10 2022
+
+@author: Sumit
+"""
+
+import random
+import hangman_pics
+import words
+
+
+choosen_word = random.choice(words.list_of_words)
+hidden_word = []
+
+for _ in choosen_word:
+    hidden_word += "_"
+    
+print(hidden_word)
+
+print("\nYou have 6 life.")
+
+life = 6
+
+while life > 0 and "_" in hidden_word:
+    guess = input("__________________________________\nGuess a letter: ")
+    for n in range(len(choosen_word)):
+        if guess in choosen_word[n]:
+            hidden_word[n] = guess
+        
+    print(hidden_word)
+    
+    
+    if not guess in hidden_word:
+        life -= 1
+        print("\nOh no, you lose a life.")
+        print(f"\nYou have {life} life remaining.")
+    print(f"{hangman_pics.h_pics[life]}")
+    if life == 0:
+        print(f"Game Over, the word is {choosen_word}")
+    if not "_" in hidden_word:
+        print("\nYou Win")
---
+++ words.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun  2 17:50:28 2022
+
+@author: Sumit
+"""
+
+list_of_words = ['ant', 'baboon', 'badger', 'bat', 'bear', 'beaver', 'camel', 'cat', 'clam', 'cobra', 'cougar',
+         'coyote', 'crow', 'deer', 'dog', 'donkey', 'duck', 'eagle', 'ferret', 'fox', 'frog', 'goat',
+         'goose', 'hawk', 'lion', 'lizard', 'llama', 'mole', 'monkey', 'moose', 'mouse', 'mule', 'newt',
+         'otter', 'owl', 'panda', 'parrot', 'pigeon', 'python', 'rabbit', 'ram', 'rat', 'raven',
+         'rhino', 'salmon', 'seal', 'shark', 'sheep', 'skunk', 'sloth', 'snake', 'spider',
+         'stork', 'swan', 'tiger', 'toad', 'trout', 'turkey', 'turtle', 'weasel', 'whale', 'wolf',
+         'wombat', 'zebra']
